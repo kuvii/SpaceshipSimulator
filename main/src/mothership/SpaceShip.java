5bbb7401a2e7b6833df3d478a260e667ad21c5ee
kuvii <kuvilet@gmail.com>
2022-04-21 09:37:59 +0100

    Feat: SpeedUP Añadido, retoques en la estructura del codigo

--- main/src/mothership/SpaceShip.java
+++ main/src/mothership/SpaceShip.java
@@ -1,21 +1,20 @@
 package mothership;
 
-public class SpaceShip {
-    private String nombre;
-    private String matriculaGalactica;
-    private float aceleracion;
-    private float velocidad_x;
-    private float velocidad_y;
-    private float coordenada_x;
-    private float coordenada_y;
-    private float direccion_x;
-    private float direccion_y;
 
-    public SpaceShip(String nombre, String matriculaGalactica, float aceleracion, float velocidad_x,
-                     float velocidad_y, float coordenada_x, float coordenada_y, float direccion_x, float direccion_y) {
-        this.nombre = nombre;
-        this.matriculaGalactica = matriculaGalactica;
-        this.aceleracion = aceleracion;
+
+public class SpaceShip {
+    private static String nombre;
+    private static String matriculaGalactica;
+    private static int aceleracion;
+    private static int velocidad_x;
+    private static int velocidad_y;
+    private static int coordenada_x;
+    private static int coordenada_y;
+    private static int direccion_x;
+    private static int direccion_y;
+
+
+    public SpaceShip(int velocidad_x, int velocidad_y, int coordenada_x, int coordenada_y, int direccion_x, int direccion_y) {
         this.velocidad_x = velocidad_x;
         this.velocidad_y = velocidad_y;
         this.coordenada_x = coordenada_x;
@@ -24,102 +23,113 @@ public class SpaceShip {
         this.direccion_y = direccion_y;
     }
 
-    public String getNombre() {
+    public static String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public static void setNombre(String nombre) {
+        SpaceShip.nombre = nombre;
     }
 
-    public String getMatriculaGalactica() {
+    public static String getMatriculaGalactica() {
         return matriculaGalactica;
     }
 
-    public void setMatriculaGalactica(String matriculaGalactica) {
-        this.matriculaGalactica = matriculaGalactica;
+    public static void setMatriculaGalactica(String matriculaGalactica) {
+        SpaceShip.matriculaGalactica = matriculaGalactica;
     }
 
-    public float getAceleracion() {
+    public static int getAceleracion() {
         return aceleracion;
     }
 
-    public void setAceleracion(float aceleracion) {
-        this.aceleracion = aceleracion;
+    public static void setAceleracion(int aceleracion) {
+        SpaceShip.aceleracion = aceleracion;
     }
 
-    public float getVelocidad_x() {
+    public static int getVelocidad_x() {
         return velocidad_x;
     }
 
-    public void setVelocidad_x(float velocidad_x) {
-        this.velocidad_x = velocidad_x;
+    public static void setVelocidad_x(int velocidad_x) {
+        SpaceShip.velocidad_x = velocidad_x;
     }
 
-    public float getVelocidad_y() {
+    public static int getVelocidad_y() {
         return velocidad_y;
     }
 
-    public void setVelocidad_y(float velocidad_y) {
-        this.velocidad_y = velocidad_y;
+    public static void setVelocidad_y(int velocidad_y) {
+        SpaceShip.velocidad_y = velocidad_y;
     }
 
-    public float getCoordenada_x() {
+    public static int getCoordenada_x() {
         return coordenada_x;
     }
 
-    public void setCoordenada_x(float coordenada_x) {
-        this.coordenada_x = coordenada_x;
+    public static void setCoordenada_x(int coordenada_x) {
+        SpaceShip.coordenada_x = coordenada_x;
     }
 
-    public float getCoordenada_y() {
+    public static int getCoordenada_y() {
         return coordenada_y;
     }
 
-    public void setCoordenada_y(float coordenada_y) {
-        this.coordenada_y = coordenada_y;
+    public static void setCoordenada_y(int coordenada_y) {
+        SpaceShip.coordenada_y = coordenada_y;
     }
 
-    public float getDireccion_x() {
+    public static int getDireccion_x() {
         return direccion_x;
     }
 
-    public void setDireccion_x(float direccion_x) {
-        this.direccion_x = direccion_x;
+    public static void setDireccion_x(int direccion_x) {
+        SpaceShip.direccion_x = direccion_x;
     }
 
-    public float getDireccion_y() {
+    public static int getDireccion_y() {
         return direccion_y;
     }
 
-    public void setDireccion_y(float direccion_y) {
-        this.direccion_y = direccion_y;
+    public static void setDireccion_y(int direccion_y) {
+        SpaceShip.direccion_y = direccion_y;
     }
 
+    public static void speedUp() {
+        setVelocidad_x(velocidad_x + aceleracion);
+        setCoordenada_x(getCoordenada_x() + getVelocidad_x());
+        System.out.println("Velocidad X: " + getVelocidad_x() + "\n" + "Coordenada X: " + getCoordenada_x());
+        if (velocidad_x > 1000){
+            System.out.println("PM Zeus: Warp Detected, stoping system");
+            System.exit(0);
+        }
+    }
+
+
     @Override
-    public String toString(){
-        String valorPrint = "*-------------"+ getNombre() + "-------------*" + "\n";
+    public String toString() {
+        String valorPrint = "*-------------" + getNombre() + "-------------*" + "\n";
         valorPrint += "Matricula Galactica: " + getMatriculaGalactica() + "\n";
         valorPrint += "Velocidad en X Actual: " + getVelocidad_x() + "\n";
         valorPrint += "Velocidad en Y Actual: " + getVelocidad_y() + "\n";
         valorPrint += "Aceleración Actual: " + getAceleracion() + "\n";
         valorPrint += "Posición Actual: " + getCoordenada_x() + " X " + getCoordenada_y() + " Y " + "\n";
         valorPrint += "Dirección Actual: " + getDireccion_x() + " X " + getDireccion_y() + " Y " + "\n";
-        valorPrint += "             ▄▒▒▒▒▒▒▒▒▒▒▒▄" + "\n";
-        valorPrint += "           ▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄" + "\n";
-        valorPrint += "         ▄▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▄" + "\n";
-        valorPrint += "       ▄▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▄" + "\n";
-        valorPrint += "      ]▒▒██▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▄▄" + "\n";
-        valorPrint += "      ]▒▒█████████▓▓▓▓▓╣╣╣╢╢╢╢▒▒▒▒▒▒▒▒▒▄▄▄" + "\n";
-        valorPrint += "        ▀████▓╣╣╫╫╫╫╫╫╫╫╫╢█████████▓▓▓▒▒▒▒▒▒▄▄▄▄▄" + "\n";
-        valorPrint += "          ▓█▓███████▓▓▓▓▓▓╣█████████▓▓▓▓▓▓█╢     ▓▓▒" + "\n";
-        valorPrint += "        ▄███▓▓╣╣╫╫╫╫╫╫╫╫╫╢█████████▓▒▒▒▒▒▒▒▒▀▀▀▀▀" + "\n";
-        valorPrint += "      ]▒███████████████╣╣╣╢╢╢╢▒▒▒▒▒▒▒▒▒▀▀▀" + "\n";
-        valorPrint += "      ]▒█████████▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▀▀" + "\n";
-        valorPrint += "       ▀▒█▓█████▓▓███████▓▒▒▒▓▒▀" + "\n";
-        valorPrint += "         ▀▒█████▓▓▓▒▒▒▒▒▒▒▒▒▒▀" + "\n";
-        valorPrint += "           ▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀" + "\n";
-        valorPrint += "             ▀▒▒▒▒▒▒▒▒▒▒▒▀" + "\n";
+        valorPrint += "|             ▄▒▒▒▒▒▒▒▒▒▒▒▄" + "\n";
+        valorPrint += "|           ▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄" + "\n";
+        valorPrint += "|         ▄▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▄" + "\n";
+        valorPrint += "|       ▄▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▄" + "\n";
+        valorPrint += "|      ]▒▒██▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▄▄" + "\n";
+        valorPrint += "|      ]▒▒█████████▓▓▓▓▓╣╣╣╢╢╢╢▒▒▒▒▒▒▒▒▒▄▄▄" + "\n";
+        valorPrint += "|        ▀████▓╣╣╫╫╫╫╫╫╫╫╫╢█████████▓▓▓▒▒▒▒▒▒▄▄▄▄▄" + "\n";
+        valorPrint += "|          ▓█▓███████▓▓▓▓▓▓╣█████████▓▓▓▓▓▓█╢     ▓▓▒" + "\n";
+        valorPrint += "|        ▄███▓▓╣╣╫╫╫╫╫╫╫╫╫╢█████████▓▒▒▒▒▒▒▒▒▀▀▀▀▀" + "\n";
+        valorPrint += "|      ]▒███████████████╣╣╣╢╢╢╢▒▒▒▒▒▒▒▒▒▀▀▀" + "\n";
+        valorPrint += "|      ]▒█████████▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▀▀" + "\n";
+        valorPrint += "|       ▀▒█▓█████▓▓███████▓▒▒▒▓▒▀" + "\n";
+        valorPrint += "|         ▀▒█████▓▓▓▒▒▒▒▒▒▒▒▒▒▀" + "\n";
+        valorPrint += "|           ▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀" + "\n";
+        valorPrint += "|             ▀▒▒▒▒▒▒▒▒▒▒▒▀" + "\n";
 
         return valorPrint;
     }
